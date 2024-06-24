a2a213a04320a6fc111172e3a8ccfeba26eb0b8e
Mariem Derbali <mariem.derbali20@gmail.com>
2024-06-24 20:21:20 +0100

    change password on first login completed

--- Client/Screens/UserAccountManagement/ChangePasswordOnFirstLogin.js
+++ Client/Screens/UserAccountManagement/ChangePasswordOnFirstLogin.js
@@ -1,109 +1,179 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Modal } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Modal, Alert } from 'react-native';
 import axios from 'axios';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-export default function ChangePasswordOnFirstLogin({ visible, onClose, userId }) {
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
+export default function ChangePasswordOnFirstLogin({ visible, onClose, userId}) {
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const [hideNewPassword, setHideNewPassword] = useState(true);
+    const [hideConfirmPassword, setHideConfirmPassword] = useState(true);
 
-  const handlePasswordChange = async () => {
-    if (newPassword !== confirmPassword) {
-      setError("Passwords do not match.");
-      return;
-    }
 
-    try {
-      const response = await axios.post('http://192.168.1.17:3000/api/user/changePassword', {
-        userId,
-        newPassword
-      });
+    const handlePasswordChange = async () => {
+        if (!newPassword || !confirmPassword) {
+            setError("Veuillez saisir un nouveau mot de passe.");
+            return;
+        }
 
-      if (response.data.success) {
-        onClose();
-      } else {
-        setError("Error changing password.");
-      }
-    } catch (error) {
-      setError("Error changing password.");
-      console.error(error);
-    }
-  };
+        if (newPassword !== confirmPassword) {
+            setError("Les mots de passe ne correspondent pas.");
+            return;
+        }
+    
+        try {
+            const response = await axios.post('http://192.168.1.17:3000/api/user/changePassword', {
+                userId,
+                newPassword
+            });
+    
+            if (response.data.success) {
+                Alert.alert(
+                    "Succès",
+                    "Le mot de passe a été changé avec succès.",
+                    [{ text: "OK", onPress: onClose }]
+                );
+            } else {
+                setError("Erreur lors du changement de mot de passe.");
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 400) {
+                setError("Le nouveau mot de passe ne peut pas être le même que l'ancien.");
+            } else {
+                setError("Erreur lors du changement de mot de passe.");
+                console.error(error);
+            }
+        }
+    };
+    const toggleNewPasswordVisibility = () => {
+        setHideNewPassword(!hideNewPassword);
+    };
 
-  return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={visible}
-      onRequestClose={() => {}}
+    const toggleConfirmPasswordVisibility = () => {
+        setHideConfirmPassword(!hideConfirmPassword);
+    };
+
+    return (
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={() => { }}
     >
-      <View style={styles.modalContainer}>
-        <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Change Password</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="New Password"
-            secureTextEntry
-            value={newPassword}
-            onChangeText={setNewPassword}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Confirm Password"
-            secureTextEntry
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
-          />
-          {error ? <Text style={styles.errorText}>{error}</Text> : null}
-          <TouchableOpacity style={styles.modalButton} onPress={handlePasswordChange}>
-            <Text style={styles.modalButtonText}>Change Password</Text>
-          </TouchableOpacity>
+        <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+                <Text style={styles.modalTitle}>Changer mot de passe</Text>
+                <Text style={styles.modalSubTitle}>Votre mot de passe a expiré. Veuillez saisir un nouveau mot de passe</Text>
+                <View style={styles.inputContainer}>
+                    <View style={styles.input}>
+                     
+                        <TextInput
+                            placeholder="Nouveau mot de passe"
+                            secureTextEntry={hideNewPassword}
+                            value={newPassword}
+                            onChangeText={setNewPassword}
+                            style={{ flex: 1 }}
+                        />
+                        <TouchableOpacity onPress={toggleNewPasswordVisibility}>
+                            <FontAwesome5
+                                name={hideNewPassword ? 'eye-slash' : 'eye'}
+                                size={20}
+                                color="#6b7280"
+                                style={styles.inputIcon}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.inputContainer}>
+                    <View style={styles.input}>
+                   
+                        <TextInput
+                            placeholder="Confirmer le mot de passe"
+                            secureTextEntry={hideConfirmPassword}
+                            value={confirmPassword}
+                            onChangeText={setConfirmPassword}
+                            style={{ flex: 1 }}
+                        />
+                        <TouchableOpacity onPress={toggleConfirmPasswordVisibility}>
+                            <FontAwesome5
+                                name={hideConfirmPassword ? 'eye-slash' : 'eye'}
+                                size={20}
+                                color="#6b7280"
+                                style={styles.inputIcon}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                {error ? <Text style={styles.errorText}>{error}</Text> : null}
+                <TouchableOpacity style={styles.modalButton} onPress={handlePasswordChange}>
+                    <Text style={styles.modalButtonText}>Changer</Text>
+                </TouchableOpacity>
+            </View>
         </View>
-      </View>
     </Modal>
-  );
+    );
 }
 
+
 const styles = StyleSheet.create({
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    padding: 20,
-    borderRadius: 10,
-    alignItems: 'center',
-    width: '80%',
-  },
-  modalTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 15,
-  },
-  input: {
-    width: '100%',
-    padding: 10,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  errorText: {
-    color: 'red',
-    marginBottom: 10,
-  },
-  modalButton: {
-    backgroundColor: '#FEE502',
-    padding: 10,
-    borderRadius: 5,
-    width: '100%',
-    alignItems: 'center',
-  },
-  modalButtonText: {
-    color: '#373737',
-    fontWeight: 'bold',
-  },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        width: '80%',
+    },
+    modalTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        color: '#977700',
+    },
+    modalSubTitle: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        marginBottom: 15,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: 10,
+    },
+    input: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 1,
+        padding: 10,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginRight: 10,
+    },
+    errorText: {
+        color: 'red',
+        marginBottom: 10,
+    },
+    modalButton: {
+        backgroundColor: '#FEE502',
+        padding: 10,
+        borderRadius: 5,
+        width: '100%',
+        alignItems: 'center',
+    },
+    modalButtonText: {
+        color: '#373737',
+        fontWeight: 'bold',
+    },
+    inputIcon: {
+        padding: 10,
+    },
 });
