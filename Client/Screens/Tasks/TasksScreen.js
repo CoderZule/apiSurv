094028378a11b772083078550134b6523348ca48
Mariem Derbali <mariem.derbali20@gmail.com>
2024-06-29 23:55:48 +0100

    Removed notif func

--- Client/Screens/Tasks/TasksScreen.js
+++ Client/Screens/Tasks/TasksScreen.js
@@ -24,63 +24,6 @@ const TasksScreen = ({ navigation }) => {
   const [errorDates, setErrorDates] = useState('');
 
 
-
-
-  //Notification
-  
-  const requestUserPermission = async () => {
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-    if (enabled) {
-      console.log('Authorization status:', authStatus);
-    }
-  }
-
-  useEffect(() => {
-    if (requestUserPermission()) {
-      messaging().getToken().then(token => {
-        console.log(token);
-      })
-
-    } else {
-      console.log("Failed token status");
-    }
-
-    messaging()
-      .getInitialNotification()
-      .then(async (remoteMessage) => {
-        if (remoteMessage) {
-          console.log('Notification caused app to open from quit state:',
-            remoteMessage.notification,
-          );
-        }
-      });
-
-    messaging().onNotificationOpenedApp(async (remoteMessage) => {
-      console.log('Notification caused app to open from background state:',
-        remoteMessage.notification,
-      );
-    });
-
-    // Register background handler
-    messaging().setBackgroundMessageHandler(async remoteMessage => {
-      console.log('Message handled in the background!', remoteMessage);
-    });
-
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
-
-    return unsubscribe;
-  }, [])
-
-
-
-  //End of notification
-
   useEffect(() => {
     LogBox.ignoreLogs([
       'Warning: Unknown: Support for defaultProps will be removed from memo components in a future major release. Use JavaScript default parameters instead.',
